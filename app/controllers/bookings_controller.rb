22d9a978128081c59aa9d5c928d8e7ec2acc7c16
Zackaria Hitane <zackhitane@gmail.com>
2021-07-27 16:19:44 +0800

    wip

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,24 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
+  end
+
+  def create
+    raise
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
   end
 
   def update
+  end
+
+  private
+
+  def booking_params
+    params.require(@booking).permit(:collection_date, :return_date)
   end
 end
