2b851453b79fd6f51f574aa3c5dd2f62e892ebde
Brian Lavery <brian.lavery@yahoo.co.uk>
2021-07-27 10:37:36 +0800

    Completed routes post pull request

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'surfboards#index'
   get '/dashboard', to: 'users#dashboard'
 
-  resources :surfboards, only: [:index, :show, :new, :create, :edit, :update] do
+  resources :surfboards, except: :destroy do
     resources :bookings, only: [:new, :create]
   end
 
