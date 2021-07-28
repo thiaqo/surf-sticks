8c02450b84fd32eaf8cc06d60b503712cc232c1c
Brian Lavery <brian.lavery@yahoo.co.uk>
2021-07-28 21:47:22 +0800

    Added name and username to user signup, allowed user edit including add photo

--- app/models/surfboard.rb
+++ app/models/surfboard.rb
@@ -3,8 +3,6 @@ class Surfboard < ApplicationRecord
   has_many :bookings
   has_many :reviews, through: :bookings
   has_one_attached :photo
-  # We will need to add the line below once we incorporate cloudinary
-  # has_one_attached :photo
 
   validates :length, :volume, :price_per_day, presence: true, numericality: { only_integer: true }
   validates :name, :board_type, :fin_type, :brand, :description, presence: true
