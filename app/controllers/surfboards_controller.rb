981f7f9a8aff14e7cd788454d384af403f7e2107
Amanuel Gebreselassie <amanuel.gselassie99@gmail.com>
2021-07-28 13:14:42 +0800

    surfboard edit and update

--- app/controllers/surfboards_controller.rb
+++ app/controllers/surfboards_controller.rb
@@ -24,6 +24,13 @@ class SurfboardsController < ApplicationController
   end
 
   def edit
+    @surfboard = Surfboard.find(params[:id])
+  end
+
+  def update
+    @surfboard = Surfboard.find(params[:id])
+    @surfboard.update(surfboard_params)
+    redirect_to surfboard_path(@surfboard)
   end
 
   private
